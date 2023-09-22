947fddf980d2e8fb33ea6a9237362650281cb360
Claudio Goncalves <claudiojmg1@hotmail.de>
2023-09-22 13:45:54 +0200

    up date  file add

---
+++ script.js
@@ -0,0 +1,16 @@
+/*let numeros = [1, 2, 3, 4];
+
+for (i = 0; i < numeros.length; i++) {
+  let numero = numeros[i];
+  if (numero % 2 != 0) {
+    console.log(numero);
+  }
+}*/
+
+function oddNumbers() {
+  for (let i = 11; i <= 33; i += 2) {
+    console.log(i);
+  }
+}
+
+oddNumbers();
